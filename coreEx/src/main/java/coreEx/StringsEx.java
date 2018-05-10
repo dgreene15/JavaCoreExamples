03eb1a85a6d9b9feba574e14aa6214ae129319de
dgreene15 <35006088+dgreene15@users.noreply.github.com>
2018-05-09 21:39:59 -0400

    Adding new string example files

---
+++ coreEx/src/main/java/coreEx/StringsEx.java
@@ -0,0 +1,22 @@
+package coreEx;
+
+public class StringsEx {
+
+	public static void main(String[] args) {
+		String fname = "FName";
+		String lname = "LName";
+
+		// StringBuffer - thread safe (java.lang)
+		StringBuffer sb = new StringBuffer("name: ");
+		sb.append(fname);
+		sb.append(lname);
+		System.out.println(sb);
+		
+		// StringBuilder - non-thread safe (java.lang)
+		StringBuilder sb2 = new StringBuilder("name: ");
+		sb2.append(fname);
+		sb2.append(lname);
+		System.out.println(sb2.toString());
+	}
+
+}
